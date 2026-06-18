3f988c0563c0bcd32985a962720d5b2062bcfa5f
LucaMarquez <lucaecheverria20@gmail.com>
2026-06-18 14:57:37 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Configuración Firebase (mismo proyecto que el panel web)
+window.FIREBASE_CONFIG = {
+  apiKey:       "AIzaSyC3wR3ExqVNLin6fWg0DMcDA5DPuSDW7fE",
+  authDomain:   "rastreo-quilu.firebaseapp.com",
+  databaseURL:  "https://rastreo-quilu-default-rtdb.firebaseio.com",
+  projectId:    "rastreo-quilu",
+  appId:        "1:228556985058:web:ca2a659c319559effd04bb"
+};
+window.CODIGO_LOGISTICA = "quiluhogar";
